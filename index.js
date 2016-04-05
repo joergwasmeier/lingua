77e8bad9a34212b2a31b68b1f8f6b2a7bb55e1bf
Jörg Wasmeier <j.wasmeier@faba.de>
2016-04-05 07:26:53 +0200

    INIT
    Test Server

--- index.js
+++ index.js
@@ -8,15 +8,6 @@ var connection = null;
 var serve = require('koa-static');
 var mount = require('koa-mount');
 
-r.connect( {host: 'localhost', port: 28015}, function(err, conn) {
-    if (err) throw err;
-    connection = conn;
-
-    r.db('test').table('authors').insert({test:"jeah"}).run(connection, function(err, result) {
-        if (err) throw err;
-    });
-
-});
 
 app.use(mount('/log', serve('/home/deploy/.pm2/logs/')));
 
@@ -28,4 +19,4 @@ app.use(function *(next){
     }
 })
 
-app.listen(3100);
+app.listen(3110);
