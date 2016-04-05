ab5486ecc31e8c95d17e6fe03578336ed945b7a6
Jörg Wasmeier <j.wasmeier@faba.de>
2016-04-05 07:52:35 +0200

    Add Test Lingua

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var mount = require('koa-mount');
 
 app.use(mount('/log', serve('/home/deploy/.pm2/logs/')));
 
-app.use(serve('/home/deploy/joerg_test-project/'));
+app.use(serve('/home/deploy/joergwasmeier_lingua/'));
 app.use(function *(next){
     yield next;
     if ('/' == this.path) {
