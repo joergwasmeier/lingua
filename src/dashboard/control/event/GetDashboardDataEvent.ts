0d4a4416027d4ef76c7cdfe917cdcb230dbee1a6
Jörg Wasmeier <info@joergwasmeier.de>
2016-04-14 11:06:10 +0200

    Insert Event Identifyer

--- src/dashboard/control/event/GetDashboardDataEvent.ts
+++ src/dashboard/control/event/GetDashboardDataEvent.ts
@@ -3,9 +3,11 @@ import FabaEvent from "fabalous-core/core/FabaEvent";
 export default class GetDashboardDataEvent extends FabaEvent{
 
   test:string;
+  identifyer:string = "GetDashboardDataEvent";
 
   constructor(){
     super();
+
     this.test = "super";
   }
 
