63e4a9e621015753ecb1f85c7d564c9872f28151
Jörg Wasmeier <j.wasmeier@faba.de>
2016-04-21 10:08:24 +0200

    Chunk hash

--- config/webpack/webpack.frontend.config.js
+++ config/webpack/webpack.frontend.config.js
@@ -4,7 +4,8 @@ var webpack = require('webpack');
 module.exports = {
     output: {
         path: path.join(__dirname,'../../dist/web/'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        chunkFilename: 'bundle-[chunkhash].js'
     },
 
     debug: true,
