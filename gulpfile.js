43d656ac2cff8d912d446cde538aa23c652667e6
Jörg Wasmeier <j.wasmeier@faba.de>
2016-05-13 17:41:57 +0200

    Css Tests

--- gulpfile.js
+++ gulpfile.js
@@ -25,10 +25,32 @@ gulp.task('cordova-build', function(){
 
 var casperJs = require('gulp-casperjs');
 
+/*
 gulp.task('testPhantomCss', function(){
     gulp.src('demo/testsuite.js')
         .pipe(casperJs({command:'test'})); //run casperjs test
 
+});
+
+*/
+
+var spawn = require('child_process').spawn,
+    gutil = require('gulp-util');
+
+gulp.task('testPhantomCss', function () {
+    var tests = ['demo/testsuite.js'];
+
+    var casperChild = spawn('casperjs', ['test'].concat(tests));
+
+    casperChild.stdout.on('data', function (data) {
+        gutil.log('CasperJS:', data.toString().slice(0, -1)); // Remove \n
+    });
+
+    casperChild.on('close', function (code) {
+        var success = code === 0; // Will be 1 in the event of failure
+
+        // Do something with success here
+    });
 });
 
 
