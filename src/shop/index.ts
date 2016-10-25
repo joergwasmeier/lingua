4279235cc45f1a3ff9157f6e16f05ced10c5a0fd
Jörg Wasmeier <j.wasmeier@faba.de>
2016-10-25 15:17:41 +0200

    Fix Build

--- src/shop/index.ts
+++ src/shop/index.ts
@@ -1,18 +1,5 @@
 import ShopMediator from "./ShopMediator";
 import InitShopEvent from "./event/InitShopEvent";
-import FabaCore from "@fabalous/core/FabaCore";
-declare var module;
-
-
-module.hot.dispose(function(){
-    var cls = require('./ShopMediator').default;
-    FabaCore.mediators = [];
-    FabaCore.mediators.push({
-        cls:cls,
-        mediator:new cls
-    });
-});
-
 
 module.exports = {
     mediator: ShopMediator,
