85cac772803552165014e6dfb41782a5cbbf0044
Jörg Wasmeier <j.wasmeier@faba.de>
2016-10-31 21:14:17 +0100

    Add Cardlist

--- src/common/widgets/cardList/CardList.tsx
+++ src/common/widgets/cardList/CardList.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import ItemList from "../itemList/ItemList";
 import {Divider} from "material-ui";
+import {Card, CardHeader, CardMedia, CardTitle, CardText} from "material-ui";
 
 export default class CardList extends ItemList{
     constructor(props){
@@ -14,6 +15,22 @@ export default class CardList extends ItemList{
         if (this.props.items){
             for (var item of this.props.items) {
                 rows.push(<Divider key={"div"+item.id}/>);
+                rows.push(
+                    <Card>
+                        <CardHeader
+                            title="URL Avatar"
+                            subtitle="Subtitle"
+                            avatar="images/jsa-128.jpg"
+                        />
+                        <CardTitle title="Card title" subtitle="Card subtitle" />
+                        <CardText>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+                            Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+                            Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+                        </CardText>
+                    </Card>
+                );
             }
         }
         return rows;
