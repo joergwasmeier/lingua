3b156bada03f9e24c4f68f1f25ff4ee76bb1195d
Jörg Wasmeier <j.wasmeier@faba.de>
2016-10-31 21:57:51 +0100

    Add Cardlist

--- src/common/widgets/cardList/CardList.tsx
+++ src/common/widgets/cardList/CardList.tsx
@@ -15,24 +15,42 @@ export default class CardList extends ItemList{
         if (this.props.items){
             for (var item of this.props.items) {
                 rows.push(<Divider key={"div"+item.id}/>);
-                rows.push(
-                    <Card>
-                        <CardHeader
-                            title="URL Avatar"
-                            subtitle="Subtitle"
-                            avatar="images/jsa-128.jpg"
-                        />
-                        <CardTitle title="Card title" subtitle="Card subtitle" />
-                        <CardText>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                            Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-                            Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-                            Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-                        </CardText>
-                    </Card>
-                );
+                rows.push(<CardListItem key={item.id} clickEvent={this.props.clickEvent} item={item} />);
+
             }
         }
         return rows;
+    }
+}
+
+interface IItemCardItemProps{
+    item:any;
+    clickEvent:any;
+}
+
+class CardListItem extends React.Component<IItemCardItemProps,{}>{
+    private itemClick(item){
+        console.log(item);
+        this.props.clickEvent.item = item;
+        this.props.clickEvent.dispatch();
+    }
+
+    render(){
+        return(
+            <Card onClick={()=>{this.itemClick(this.props.item);}}>
+                <CardHeader
+                    title="URL Avatar"
+                    subtitle="Subtitle"
+
+                />
+                <CardTitle title="Card title" subtitle="Card subtitle"  />
+                <CardText>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+                    Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+                    Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+                </CardText>
+            </Card>
+        )
     }
 }
